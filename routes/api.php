c2c648223c762130192924608fdf9c308b783038
WaveHack <email@wavehack.net>
2018-02-20 21:51:47 +0100

    Use $router in api and web route files

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/** @var \Illuminate\Routing\Router $router */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//$router->middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
