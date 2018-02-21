289f338989a16c956544830d2b6984f57407b41e
WaveHack <email@wavehack.net>
2018-02-21 23:08:28 +0100

    Add some API discoverability

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,23 @@ use Illuminate\Routing\Router;
 
 /** @var \Illuminate\Routing\Router $router */
 
+$router->group(['prefix' => 'api'], function (Router $router) {
+
+    // API discoverability, yay
+    $router->get('/', function (Request $request) {
+        return json_encode(['apis' => [
+            'v1' => ['link' => url('api/v1'), 'format' => 'JSON API'],
+        ]]);
+    });
+
+    $router->get('/v1', function (Request $request) {
+        return json_encode(['resources' => [
+            'achievements' => url('api/v1/achievements'),
+        ]]);
+    });
+
+});
+
 JsonApi::register('v1', ['namespace' => 'Api\V1'], function (ApiGroup $api, Router $router) {
 
     $api->resource('achievements', [
