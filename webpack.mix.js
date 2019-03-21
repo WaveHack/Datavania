8caf074da1d6e47befb425b94b4fecac36d6c5b3
WaveHack <email@wavehack.net>
2019-03-21 10:59:09 +0100

    Cleanup webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,5 @@
 let mix = require('laravel-mix');
 
-// mix.setPublicPath('public');
-
-const vendorDirs = {
-    //
-};
-
-for (const dir in vendorDirs) {
-    mix.copyDirectory(dir, vendorDirs[dir]);
-}
-
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
