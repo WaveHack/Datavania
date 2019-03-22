a71beaf857a8bdb70ad103d5fec21fa32545345b
WaveHack <email@wavehack.net>
2019-03-23 00:16:48 +0100

    Add webpack-spritesmith and build items spritesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,9 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Prefetch spritesheets -->
+    <link rel="prefetch" href="/images/spritesheets/items.png" as="image">
 </head>
 <body>
 <div id="app">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,23 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
+const Spritesmith = require('webpack-spritesmith');
+
+mix.webpackConfig({
+    plugins: [
+        new Spritesmith({
+            src: {
+                cwd: path.resolve(__dirname, 'resources/images/icons'),
+                glob: '**/*.png',
+            },
+            target: {
+                image: path.resolve(__dirname, 'public/images/spritesheets/items.png'),
+                css: path.resolve(__dirname, 'resources/build/spritesheet-items.sass'),
+            },
+            apiOptions: {
+                cssImageRef: '/images/spritesheets/items.png',
+            }
+        })
+    ],
+});
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
