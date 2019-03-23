818558fbb871fcefc17b352b8bae60da4266fe49
WaveHack <email@wavehack.net>
2019-03-23 02:43:55 +0100

    Text change

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-6">
                 @include('partials.global-search')
                 <p class="text-muted text-right mt-1 pr-2">
-                    Press '/' to focus the search bar <i class="fa fa-level-up-alt"></i>
+                    Press / to focus the search bar <i class="fa fa-level-up-alt" style="margin-right: 10px;"></i>
                 </p>
             </div>
         </div>
